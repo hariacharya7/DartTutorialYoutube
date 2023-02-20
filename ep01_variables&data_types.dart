2c975f85272d3ac98a96a9f2e394b098874c0525
Hari Acharya <118670580+hariacharya7@users.noreply.github.com>
2023-02-20 06:20:49 +0545

    Create ep01_variables&data_types.dart
    Variables & Data Types in dart

---
+++ ep01_variables&data_types.dart
@@ -0,0 +1,11 @@
+void main(){
+ // Datatype VariableName = Value;
+  String companyName = "Google";
+  int companyPhone = 9839384;
+  double companySize = 90.5;
+  bool isCompanyNepali = false; 
+  // Text Value = String
+  // Number Value = int
+  // Decimal Value(Number Value(.)) = double
+  // True or False Value = bool
+}
